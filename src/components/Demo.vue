55e7fafed3882643acc8153b7c298329546738ac
lcy8417 <77670414+lcy8417@users.noreply.github.com>
2021-05-20 23:49:23 +0900

    Add files via upload

---
+++ src/components/Demo.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="root">
+      <p>{{ originalPrice }}</p>
+      <p>{{ doublePrice }}</p>
+      <p>{{ triplePrice }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+    computed: {
+        originalPrice(){
+            return this.$store.getters.originalPrice;
+        },
+        doublePrice(){
+            return this.$store.getters.doublePrice;
+        },
+        triplePrice() {
+            return this.$store.getters.triplePrice;
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
